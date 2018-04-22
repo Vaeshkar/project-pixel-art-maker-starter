06c66f705d80de7f0e1f66c1b26e0da237b49402
Dennis van Leeuwen <bendora@gmail.com>
2018-04-22 18:04:26 +0200

    added JS code and simple tweak in HTML

--- designs.js
+++ designs.js
@@ -1,10 +1,37 @@
-// Select color input
-// Select size input
+// Grid Variable
+var gridHeight, gridWidth;
 
-// When size is submitted by the user, call makeGrid()
-
+// Grid method
 function makeGrid() {
 
-// Your code goes here!
+    // Grab DOM Table
+    var table = document.getElementById("pixelCanvas");
+    
+    // Reset the old Grid
+    var Parent = document.getElementById("pixelCanvas");
+    while (Parent.hasChildNodes()) {
+        Parent.removeChild(Parent.firstChild);
+    }
 
+    // Grid generator
+    gridHeight = document.getElementById("inputHeight").value;
+    gridWidth = document.getElementById("inputWidth").value;
+    
+    // Loop to generate the Grid
+    for (let i = 0; i < gridHeight; i++) {
+        let tableRow = table.insertRow(i);
+        for (let j = 0; j < gridWidth; j++) {
+            let tableColumn = tableRow.insertCell(j);
+    // Drawing check
+            tableColumn.addEventListener('click', function(event) {
+          event.target.style.backgroundColor = document.getElementById("colorPicker").value;
+            });
+            
+        }
+    }
 }
+
+document.getElementById("sizePicker").addEventListener("submit", function(event) {
+    event.preventDefault();
+    makeGrid();
+});
